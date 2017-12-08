ead4afca015dc31ff506b78567b73b8a4e2cb2b2
Mike Grabski <me@mikegrabski.com>
2017-12-07 19:57:21 -0500

    load country data from JSON file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,23 @@
 $(function() {
     // create an array of country names/codes
-    var countries = [
-        { name: 'Belarus', code: '112'},
-        { name: 'United Kingdom', code: '826'},
-        { name: 'United States of America', code: '840'},
-    ];
+    var countries;
 
-    $('#country').typeahead({
-        source: countries
-    })
-    .change(function() {
-        var country = $(this).typeahead("getActive");
-        console.log(country);
-        $('#result').show();
-        $('#country-name').text(country.name);
-        $('#country-code').text(country.code);
+    $.ajax({
+        method: 'GET',
+        url: 'assets/data/countries.json',
+        dataType: 'json'
+    }).done(function(data) {
+        countries = data;
+
+        $('#country').typeahead({
+            source: countries
+        })
+        .change(function () {
+            var country = $(this).typeahead("getActive");
+            console.log(country);
+            $('#result').show();
+            $('#country-name').text(country.name);
+            $('#country-code').text(country.alpha3Code);
+        });
     });
-
-
 });
